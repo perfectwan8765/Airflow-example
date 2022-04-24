fbfd3141e6ee2802bf5a63bb1f52813c91cda4a6
jsw <perfectwan8765@gmail.com>
2022-04-24 15:34:34 +0900

    UPDATE sqlqueires Table DDL

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,29 +1,22 @@
 class SqlQueries:
     create_table_list = ['''
-            CREATE TABLE IF NOT EXISTS bts_toptrack (
-                market varchar(5) not null,
-                rank integer not null,
-                track_id varchar(100) not null,
-                track_name varchar(100) not null,
-                popularity integer not null,
-                duration_ms integer not null,
-                track_number integer not null,
-                album_id varchar(100) not null,
-                album_name varchar(100) not null,
-                release_date date not null,
-                primary key(market, track_id)
+            CREATE TABLE IF NOT EXISTS celerbrity (
+	            id bigint not null
+	          , celerbrity varchar(256)
+	          , followers_count integer
+	          , url varchar(256)
+	          , check_date timestamp not null
+	          , primary key (id, check_date)
             )
             ''',
             '''
-            CREATE TABLE IF NOT EXISTS bts_toptrack_tweet (
-                tweet_id bigint not null,
-                track_name varchar(100) not null,
-                tweet_url varchar(300) not null,
-                created_at timestamp not null,
-                user_id bigint not null,
-                user_name varchar(200) not null,
-                user_followers_count varchar(200) not null,
-                primary key(tweet_id)
+            CREATE TABLE IF NOT EXISTS celerbrity_tweet (
+                twit_id bigint
+	          , celebrity varchar(256)
+	          , tweet_url varchar(256)
+	          , retweet_count integer
+	          , favorite_count integer
+	          , created_at timestamp
             )
             '''
     ]
